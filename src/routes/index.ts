7a68aa7fc19b0f29d3f205766e54c84bc3c79cb5
Guilherme <guilhermehallmann@gmail.com>
2022-06-13 17:57:22 -0300

    feat: creating user interface

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,7 +5,11 @@ import productValidation from '../middlewares/productValidation';
 const router = Router();
 const Product = new ProductController();
 
+// Product Routes
 router.get('/products', Product.getAll);
 router.post('/products', productValidation, Product.create);
+
+// User Routes
+router.post('/users');
 
 export default router;
