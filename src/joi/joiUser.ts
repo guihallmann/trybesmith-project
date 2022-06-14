ec7968418686e714de3bb751602bc7e12b3354de
Guilherme <guilhermehallmann@gmail.com>
2022-06-14 13:13:01 -0300

    feat: user registration

--- src/joi/joiUser.ts
+++ src/joi/joiUser.ts
@@ -1,8 +1,8 @@
 import Joi from 'joi';
 
 const joiUser = Joi.object({
-  username: Joi.string().min(2).required(),
-  classe: Joi.string().min(2).required(),
+  username: Joi.string().min(3).required(),
+  classe: Joi.string().min(3).required(),
   level: Joi.number().integer().min(1).required(),
   password: Joi.string().min(8).required(),
 });
